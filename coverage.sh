3452ac6aaec2ca24a9323edf422f317e2ebe6f05
Alan McGinlay <amcginlay@gmail.com>
2016-09-09 00:49:27 +0100

    first attempt at publishing coverage to coveralls

---
+++ coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# https://github.com/nickmerwin/node-coveralls
+# script based on linked document:
+# https://www.npmjs.com/package/mocha-lcov-reporter
+
+#!/usr/bin/env bash 
+rm -rf coverage
+rm -rf app-cov
+ 
+node_modules/.bin/jscover app app-cov
+mv app app-orig
+mv app-cov app
+node_modules/.bin/mocha app/tests -R mocha-lcov-reporter | node_modules/coveralls/bin/coveralls.js app
+rm -rf app
+mv app-orig app
