ee670607379df8fb1f352aa187ce02cd3c03986a
Alan McGinlay <amcginlay@gmail.com>
2016-09-11 13:19:15 +0100

    now loads classic via bower

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,8 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-                'public/lib/jquery/dist/jquery.min.js'
+				//'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/classie/classie.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
