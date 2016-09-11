dda19ae9397c540f2e2c9bd7f8d3f324f89f6ce7
Alan McGinlay <amcginlay@gmail.com>
2016-09-11 13:54:19 +0100

    include query in config assets, otherwise karma tests fail

--- config/env/all.js
+++ config/env/all.js
@@ -26,7 +26,8 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				//'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/jquery.scrollTo/jquery.scrollTo.js',
 				'public/lib/jquery.localScroll/jquery.localScroll.js',
                 'public/lib/classie/classie.js'
 			]
